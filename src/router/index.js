42e191fb1c3604b0997f9b479d6e6ce2b5738f91
晚安 <2963017296@qq.com>
2026-09-08 22:21:01 +0800

    feat: 前台路由改为 history 模式，去除 URL 中的 #

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { useUserStore } from '../stores/user'
 
 const routes = [
@@ -87,7 +87,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 }
